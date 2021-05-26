efb50e71478abc235be629621d5881d2fb055863
Ashee <grandchancellor21@outlook.com>
2021-05-27 00:42:22 +0500

    My first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,81 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: MyHomePage(title: 'My First Assignment'),
-    );
+      home: HomeScreen(title: 'My First Assignment'),
+   );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+  }
+class _MyHomePageState extends State<HomeScreen> {
   int _count = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _count++;
-    });//setState
-  }
-  //incrementCounter
+     void _incrementCounter() {
+       setState(() {
+         _count++;
+       });
+     }
   void _decrementCounter() {
     setState(() {
       if(_count==0){
-         return;
+        return;
       }//if
       _count--;
-    });//setState
+    });
   }
-   //decrementCounter
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter and _decrementCounter methods above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+          title: Text('My Counter App'),
+          backgroundColor: Colors.deepPurple),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_count',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: Text('$_count',style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold)),
       ),
-      floatingActionButtonLocation:
-      FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            FloatingActionButton(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      // Added row with 2 buttons
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 35,bottom: 15),
+            child: FloatingActionButton(
+              backgroundColor: Colors.deepPurple,
+              child: Icon(
+                Icons.remove,
+              ),
+              onPressed: _decrementCounter ,
+              tooltip: 'Decrement',
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 17),
+            child: FloatingActionButton(
+              backgroundColor: Colors.deepPurple,
+              child: Icon(
+                Icons.add,
+              ),
               onPressed: _incrementCounter,
               tooltip: 'Increment',
-               child: Icon(Icons.add),
             ),
-            FloatingActionButton(
-              onPressed: _decrementCounter,
-              tooltip: 'Decrement',
-              child: Icon(Icons.remove),
-            )
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        ],
+      ),
 
     );
   }
+}
+
+class HomeScreen  extends StatefulWidget{
+  HomeScreen({Key? key, required this.title}) : super(key: key);
+  final String title;
+     @override
+     _MyHomePageState createState() => _MyHomePageState();
 }
